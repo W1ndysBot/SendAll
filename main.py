483ef03c7a339bc6946abb56c20916b73a4a8bfc
W1ndys <w1ndys@outlook.com>
2024-08-26 08:45:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,31 +21,17 @@ DATA_DIR = os.path.join(
 )
 
 
-# 检查是否是群主
-def is_group_owner(role):
-    return role == "owner"
-
-
-# 检查是否是管理员
-def is_group_admin(role):
-    return role == "admin"
-
-
-# 检查是否有权限（管理员、群主或root管理员）
-def is_authorized(role, user_id):
-    is_admin = is_group_admin(role)
-    is_owner = is_group_owner(role)
-    return (is_admin or is_owner) or (user_id in owner_id)
-
-
-
-
 # 私聊消息处理函数
 async def handle_SendAll_private_message(websocket, msg):
     try:
         user_id = str(msg.get("user_id"))
         raw_message = str(msg.get("raw_message"))
 
+        if raw_message.startwith("send"):
+			if user_id not in owner_id:
+            	send_private_msg(websocket,user_id,f"你没有权限执行群发命令")
+                return
+            
     except Exception as e:
         logging.error(f"处理xxx私聊消息失败: {e}")
         return
